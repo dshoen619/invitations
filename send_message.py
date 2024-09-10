c788cef0065147ed09b3aa0c173a36caec53f4a0
dshoen619 <107557675+dshoen619@users.noreply.github.com>
2024-09-10 20:22:13 +0300

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -14,15 +14,16 @@ def twilio_message(csid, bdy, recipient, content_variables):
 
   message = client.messages.create(
     content_sid=csid,
-    messaging_service_sid=MESSAGING_SERVICE_ID,
+    messaging_service_sid=MESSAGING_SERVICE_SID,
     content_variables=content_variables,
-    to=f'whatsapp:{recipient}'
+    to=f'whatsapp:{recipient}',
   )
 
 
 def send_whatsapp(record):
     msg_counter = 0
     phone_number = record['fields']['phone_number']
+    print(phone_number)
     id = record['id']
     twilio_dict ={ENGLISH_INVITATION_TEMPLATE: 'Hope to See you there! Please click the link to RSVP :)',
                   HEBREW_INVITATION_TEMPLATE:'נשמח לראותכם! אנא לחץ/י על הקישור כדי להגיב :)',
@@ -41,7 +42,33 @@ def send_whatsapp(record):
         twilio_message(content_sid, body, phone_number, content_variables)
         msg_counter +=1
 
+def bus_rsvp(record):
+    phone_number = record['fields']['number']
+    print(phone_number)
+    twilio_message(BUS_RSVP_SID,bdy=None,recipient=phone_number,content_variables=None)
 
+def send_invitation():
+  airtable = Airtable(base_id, table_name, api_key)
+      
+    # Fetch all records from the table
+  records = airtable.get_all()
+
+  # Print the records
+  for record in records:
+      # print(record['fields']['phone_number'])
+      if record['fields']['invited'] == '0':
+        send_whatsapp(record)
+        airtable.update(record['id'], {'invited': '1'})
+
+def send_bus_rsvp():
+     airtable = Airtable(base_id, bus_table, api_key)
+     records = airtable.get_all()
+
+     for record in records:
+      # print(record['fields']['phone_number'])
+      if record['fields']['invited'] == '0':
+        bus_rsvp(record)
+        airtable.update(record['id'], {'invited': '1'})
 
 if __name__ == "__main__":
     basedir = path.abspath(path.dirname(__file__))
@@ -51,6 +78,7 @@ if __name__ == "__main__":
     api_key =       os.getenv('AIRTABLE_API_KEY')
     base_id =       os.getenv('AIRTABLE_BASE_ID')
     table_name =    os.getenv('AIRTABLE_TABLE_NAME')
+    bus_table =     os.getenv('BUS_TABLE')
 
     # image Paths 
     english_invation_path =   os.getenv('ENGLISH_INVITATION_PATH')
@@ -63,18 +91,12 @@ if __name__ == "__main__":
     ENGLISH_INVITATION_TEMPLATE =   os.getenv('ENGLISH_INVITATION_TEMPLATE')
     HEBREW_INVITATION_TEMPLATE =    os.getenv('HEBREW_INVITATION_TEMPLATE')
     SITE_ADDRESS =                  os.getenv('SITE_ADDRESS')
-    MESSAGING_SERVICE_ID =          os.getenv('MESSAGING_SERVICE_ID')
+    MESSAGING_SERVICE_SID =         os.getenv('MESSAGING_SERVICE_SID')
+    CARD_SID =                      os.getenv('CARD_SID')
+    BUS_RSVP_SID =                  os.getenv('BUS_RSVP_TEMPLATE')
 
-
-     # Initialize image list
+    # Initialize image list
     img_list = [english_invation_path, hebrew_invitation_path]
 
-    # Initialize the Airtable object
-    airtable = Airtable(base_id, table_name, api_key)
 
-    # Fetch all records from the table
-    records = airtable.get_all()
-
-    # Print the records
-    for record in records:
-        send_whatsapp(record)
+    send_invitation()
